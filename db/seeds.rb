5814966ecdd5c53ee3a6b6d5ed125b4f741290ba
Ryan Duchin <ryanduchin@gmail.com>
2015-06-10 10:42:11 -0700

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,8 @@ c4 = u1.playlists.create!(name: 'Pop')
 p1 = u1.posts.create!(title: 'GoogleSong', url: 'google.com', artist: 'Googler')
 p2 = u1.posts.create!(title: 'FacebookSong', url: 'facebook.com', artist: 'Facebooker')
 p3 = u1.posts.create!(title: 'YahooSong', url: 'yahoo.com', artist: 'Yahoooooer')
+
+j1 = PlaylistPost.create!(playlist_id: c1.id, post_id: p1.id)
+j2 = PlaylistPost.create!(playlist_id: c1.id, post_id: p2.id)
+j3 = PlaylistPost.create!(playlist_id: c2.id, post_id: p2.id)
+j4 = PlaylistPost.create!(playlist_id: c3.id, post_id: p3.id)
