066a166b886153a6889d82026c8683e306069047
Ryan Duchin <ryanduchin@gmail.com>
2015-06-11 15:02:10 -0700

    switched to cleaner AJAX request for vimeo thumbnail

--- app/assets/javascripts/views/posts/thumbnail.js
+++ app/assets/javascripts/views/posts/thumbnail.js
@@ -9,20 +9,4 @@ VMCApp.Views.PostThumbnail = Backbone.View.extend({
     return this;
   },
 
-  vimeoLoadingThumb: function (id) {
-    var vidId = id;
-    var url = "http://vimeo.com/api/v2/video/" + vidId + ".json?callback=this.VMCApp.Views.PostThumbnail.prototype.vimeoShowThumb";
-    var id_img = "#vimeo-" + vidId;
-
-    var script = document.createElement( 'script' );
-    script.type = 'text/javascript';
-    script.src = url;
-    $(id_img).before(script);
-   },
-
-
-   vimeoShowThumb: function (data){
-     var id_img = "#vimeo-" + data[0].id;
-     $(id_img).attr('src',data[0].thumbnail_medium);
-   },
 });
