0a3b898d27c02d7b96780cc13d048e09f089c61e
Ryan Duchin <ryanduchin@gmail.com>
2015-06-12 11:49:17 -0700

    fixed playlist show double render

--- app/assets/javascripts/views/playlists/show.js
+++ app/assets/javascripts/views/playlists/show.js
@@ -4,19 +4,22 @@ VMCApp.Views.PlaylistShow = Backbone.CompositeView.extend({
 
   initialize: function () {
     this.collection = this.model.posts();
+    this.renderPosts();
     this.listenTo(this.model, "sync", this.render);
+    this.listenTo(this.model, "add", this.addPost);
   },
 
   render: function () {
     var content = this.template({ playlist: this.model });
     this.$el.html(content);
-    this.renderPosts();
     this.attachSubviews();
     return this;
   },
 
   renderPosts: function () {
-    this.collection.forEach(function (post) { this.addPost(post); }.bind(this));
+    this.collection.forEach(function (post) {
+      this.addPost(post);
+    }.bind(this));
   },
 
   addPost: function (post) {
