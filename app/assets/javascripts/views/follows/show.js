b1f060c5e7e7de0d7fd969cba29a2a537ae8589f
Ryan Duchin <ryanduchin@gmail.com>
2015-06-12 17:55:58 -0700

    created follow view

---
+++ app/assets/javascripts/views/follows/show.js
@@ -0,0 +1,48 @@
+VMCApp.Views.FollowShow = Backbone.View.extend({
+  className: 'follow-show',
+  template: JST['follows/show'],
+
+  events: {
+    "click button.btn-follow" : "toggleFollow",
+  },
+
+  render: function () {
+    var content = this.template({ model: this.model });
+    this.$el.html(content);
+    this.setBtnColor();
+    return this;
+  },
+
+  toggleLike: function () {
+    if (this.model.isFollowed()) {
+      this.unfollow();
+    } else {
+      this.follow(); //
+    }
+  },
+
+  follow: function () { // how do i differentiate with model type??
+    this.model.follow().save({ post_id: this.model.id });
+  },
+
+  unfollow: function () {
+    var follow = this.model.follow();
+    follow.destroy();
+    follow.clear();
+  },
+
+  setBtnColor: function () {
+    if (this.model.isLiked()) {
+      this.$('button.btn-follow').addClass('btn-primary');
+      this.$('button.btn-follow').html("<i class='fa'>Followed!</i>");
+    } else {
+      this.$('button.btn-follow').removeClass('btn-primary');
+      this.$('button.btn-follow').html('Follow');
+
+    }
+  },
+
+// need to change models of playlist/user to copy likes added to post model
+// need to copy view adding subview of button (copy postshow->like to playlist/user->follow)
+// now I also need a user model!
+});
