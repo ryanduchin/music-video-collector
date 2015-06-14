3e746ccd5b9c96c597475343a1009b97f996dc53
Ryan Duchin <ryanduchin@gmail.com>
2015-06-14 16:25:31 -0700

    fixed vevo height problem by adding height 225px to .thumb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,22 +8,22 @@ c4 = u2.playlists.create!(name: 'Pop')
 
 
 
-p1 = u1.posts.create!(title: 'Generate', url: 'http://www.vevo.com/watch/eric-prydz/generate/GBUV71500138', artist: 'Eric Prydz', staff: true)
 p2 = u1.posts.create!(title: 'Peace of Mind', url: 'https://www.youtube.com/watch?v=eilZug1R4Es', artist: 'Above & Beyond')
 p3 = u1.posts.create!(title: 'Melba', url: 'https://www.youtube.com/watch?v=QYvZ8TUyn9w', artist: 'Sunny Lax')
 p4 = u1.posts.create!(title: 'Ruby', url: 'https://www.youtube.com/watch?v=LH_15HVH5Yc', artist: 'Cirez D')
 p5 = u1.posts.create!(title: 'Greyhound', url: 'https://vimeo.com/75755258', artist: 'Swedish House Mafia')
+p1 = u1.posts.create!(title: 'Generate', url: 'http://www.vevo.com/watch/eric-prydz/generate/GBUV71500138', artist: 'Eric Prydz', staff: true)
 p6 = u1.posts.create(title: 'Monument', url: 'https://www.youtube.com/watch?v=6c-RbGZBnBI', artist: 'Royskopp & Robyn')
-p7 = u2.posts.create(title: 'Dancing On My Own', url: 'https://www.youtube.com/watch?v=SW05tcG3Fgw', artist: 'Robyn', staff: true)
-p8 = u2.posts.create(title: 'Chasing Time', url: 'https://www.youtube.com/watch?v=jtTjzDTpx8o', artist: 'Azelia Banks')
+p7 = u1.posts.create(title: 'Dancing On My Own', url: 'https://www.youtube.com/watch?v=SW05tcG3Fgw', artist: 'Robyn', staff: true)
+p8 = u1.posts.create(title: 'Chasing Time', url: 'https://www.youtube.com/watch?v=jtTjzDTpx8o', artist: 'Azelia Banks')
 
 
 
-j1 = PlaylistPost.create!(playlist_id: c1.id, post_id: p1.id)
 j2 = PlaylistPost.create!(playlist_id: c1.id, post_id: p2.id)
 j3 = PlaylistPost.create!(playlist_id: c2.id, post_id: p2.id)
 j4 = PlaylistPost.create!(playlist_id: c2.id, post_id: p3.id)
 j5 = PlaylistPost.create!(playlist_id: c3.id, post_id: p4.id)
+j1 = PlaylistPost.create!(playlist_id: c1.id, post_id: p1.id)
 j6 = PlaylistPost.create!(playlist_id: c1.id, post_id: p5.id)
 j7 = PlaylistPost.create!(playlist_id: c4.id, post_id: p6.id)
 j8 = PlaylistPost.create!(playlist_id: c4.id, post_id: p7.id)
