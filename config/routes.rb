faeca8057fa7122b64c535a72c4437b0abd6179b
Ryan Duchin <ryanduchin@gmail.com>
2015-06-15 13:26:08 -0700

    fixed messy routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,15 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :posts, except: [:new, :edit, :index]
     get ":filter/posts", to: "posts#index"
-    resources :playlists, except: [:new, :edit]
+
+    resources :playlists, except: [:new, :edit, :index]
     get ":filter/playlists", to: "playlists#index"
+
     resources :likes, except: [:new, :edit]
     resources :follows, except: [:new, :edit]
-    resources :users, only: [:index, :show]
+    resources :users, only: [:show]
+    get ":filter/users", to: "users#index"
+
   end
 
 
