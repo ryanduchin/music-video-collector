783a4cdc08c10b7582093612f255d31a704148c3
Ryan Duchin <ryanduchin@gmail.com>
2015-06-15 22:53:31 -0700

    has one through association

--- app/models/playlist_post.rb
+++ app/models/playlist_post.rb
@@ -3,4 +3,6 @@ class PlaylistPost < ActiveRecord::Base
   validates_uniqueness_of :post_id, scope: :playlist_id
   belongs_to :playlist
   belongs_to :post
+  has_one :owner, through: :playlist, source: :user
+
 end
