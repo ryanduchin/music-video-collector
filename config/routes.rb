bfd36d7319b40bc009ee65d38ceabe9d26ccae42
Ryan Duchin <ryanduchin@gmail.com>
2015-06-21 23:15:28 -0700

    working login and signup functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "static_pages#root"
 
-  resources :users, only: [:create]
+  resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
 
   namespace :api, defaults: { format: :json } do
